909f127878416c16e43926cc38ee15284ad2f7db
Yugant Joshi <joshi.yugant@gmail.com>
2017-12-10 00:56:58 -0500

    implement parser extra

--- sorter.c
+++ sorter.c
@@ -1,6 +1,239 @@
 #include "sorter.h"
 #include "mergesort.c"
 
+Records* input;
+
+void parseExtra(char *line, int s){
+
+        int i=0;
+        int j=0;
+        char token [300];
+        int commas =0;
+        if(line[i]==',') {
+                commas++;
+                if(commas ==1)
+                {
+                        if(line[0]== 'C') {
+                                strcpy(input[s].color, "Color");
+                        }else if(line[1]== 'B' ) {
+                                strcpy(input[s].color, "Black and White");
+                        }else{
+                                strcpy(input[s].color, " ");
+                        }
+                }
+                switch (commas) {
+
+
+                case 2:
+                        // printf("this is director token %s\n",token);
+                        strncpy(input[s].director_name, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is director name %s\n",input[s].director_name);
+                        break;
+
+                case 3:
+                        input[s].num_critic_for_reviews=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is num critics %i\n",input[s].num_critic_for_reviews);
+                        break;
+
+                case 4:
+                        input[s].duration=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is duration %i\n",input[s].duration);
+                        break;
+
+                case 5:
+                        input[s].director_facebook_likes=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is director facebook likes %i\n",input[s].director_facebook_likes);
+                        break;
+
+                case 6:
+                        input[s].actor_3_facebook_likes=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is actor 3 fb likes %i\n",input[s].actor_3_facebook_likes);
+                        break;
+
+                case 7:
+                        strncpy(input[s].actor_2_name, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is actor 2 name %s\n",input[s].actor_2_name);
+                        break;
+
+                case 8:
+                        input[s].actor_1_facebook_likes=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is actor 1 fb likes %i\n",input[s].actor_1_facebook_likes);
+                        break;
+
+                case 9:
+                        input[s].gross=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is gross %i\n",input[s].gross);
+                        break;
+
+                case 10:
+                        strncpy(input[s].genres, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is genre %s\n",input[s].genres);
+                        break;
+
+                case 11:
+                        strncpy(input[s].actor_1_name, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is actor 1 name %s\n",input[s].actor_1_name);
+                        break;
+
+                case 12:
+                        while(!isdigit(line[i+1])) {
+
+
+
+                                if(line[i]== ',') {
+                                        //printf("i should make it in here after second I \n");
+                                        token[j]=' ';
+                                }else if(line[i]== '"') {
+                                        token[j]=' ';
+
+                                }
+
+                                else{
+                                        token[j]=line[i];
+                                }
+
+
+                                j++;
+                                i++;
+
+
+                        }
+
+
+                        strncpy(input[s].movie_title, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is movie title %s\n",input[s].movie_title);
+                        break;
+
+                case 13:
+                        input[s].num_voted_users=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is num voted users %i\n",input[s].num_voted_users);
+                        break;
+
+                case 14:
+                        input[s].cast_total_facebook_likes=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is cast total facebook likes %i\n",input[s].cast_total_facebook_likes);
+                        break;
+
+                case 15:
+                        strncpy(input[s].actor_3_name, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is actor 3 name %s\n",input[s].actor_3_name);
+                        break;
+
+                case 16:
+                        input[s].facenumber_in_poster=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is face num in poster %i\n",input[s].facenumber_in_poster);
+                        break;
+
+                case 17:
+                        strncpy(input[s].plot_keywords, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is plot keywords %s\n",input[s].plot_keywords);
+                        break;
+
+                case 18:
+                        strncpy(input[s].movie_imdb_link, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is imbd link %s\n",input[s].movie_imdb_link);
+                        break;
+
+                case 19:
+                        input[s].num_user_for_reviews=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is num user for review %i\n",input[s].num_user_for_reviews);
+                        break;
+
+                case 20:
+                        strncpy(input[s].language, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is language %s\n",input[s].language);
+                        break;
+
+                case 21:
+                        strncpy(input[s].country, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is language %s\n",input[s].country);
+                        break;
+
+                case 22:
+                        strncpy(input[s].content_rating, token, 300);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is content rating %s\n",input[s].content_rating);
+                        break;
+
+                case 23:
+                        input[s].budget=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is budget %i\n",input[s].budget);
+                        break;
+
+                case 24:
+                        input[s].title_year=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is title year %i\n",input[s].title_year);
+                        break;
+
+                case 25:
+                        input[s].actor_2_facebook_likes=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is actor 2 facebook likes %i\n",input[s].actor_2_facebook_likes);
+                        break;
+
+                case 26:
+                        input[s].imdb_score=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is imdb %i\n",input[s].imdb_score);
+                        break;
+
+                case 27:
+                        input[s].aspect_ratio=atoi(token);
+                        memset(token, 0, 300);//empties token array
+                        //printf("this is aspect ratio %i\n",input[s].aspect_ratio);
+                        break;
+
+
+
+
+                }
+                j=0;
+                i++;
+
+
+        }
+        else{
+
+                if(line[i]=='a'||line[i]=='A'||line[i]=='b'||line[i]=='B' || line[i]=='c'||line[i]=='C'||line[i]=='d'||line[i]=='D'||line[i]=='e'||line[i]=='E'||
+                   line[i]=='f'||line[i]=='F'||line[i]=='g'||line[i]=='G' || line[i]=='h'||line[i]=='H'||line[i]=='i'||line[i]=='I'||line[i]=='j'||line[i]=='J'||
+                   line[i]=='k'||line[i]=='K'||line[i]=='l'||line[i]=='L' || line[i]=='m'||line[i]=='M'||line[i]=='n'||line[i]=='N'||line[i]=='o'||line[i]=='O'||
+                   line[i]=='p'||line[i]=='P'||line[i]=='q'||line[i]=='Q' || line[i]=='r'||line[i]=='R'||line[i]=='s'||line[i]=='S'||line[i]=='t'||line[i]=='T'||
+                   line[i]=='u'||line[i]=='U'||line[i]=='v'||line[i]=='V' || line[i]=='w'||line[i]=='W'||line[i]=='x'||line[i]=='X'||line[i]=='y'||line[i]=='Y'||
+                   line[i]=='z'||line[i]=='Z'||line[i]==' '||line[i]=='?'||line[i]=='_'||line[i]=='-'||line[i]=='.'||line[i]=='/'||line[i]=='|'||line[i]=='?'||
+                   line[i]=='1'||line[i]=='2'||line[i]=='3'||line[i]=='4'||line[i]=='5'||line[i]=='6'||line[i]=='7'||line[i]=='8'||line[i]=='9'||line[i]=='0'
+                   ) {
+
+                        token[j]=line[i];
+                        j++;
+                }
+                i++;
+
+        }
+        printf("S after: %d\n", s);
+}
+
 void print_csv_file(Records** finalInput, int arraySize){
         char fileName[] = "sortedMovies.csv";
         FILE *file = fopen(fileName, "w");
@@ -282,8 +515,7 @@ int findColumnIndex(const char* value){
 
 int main(int argc, char **argv) {
 
-        Records* input= (Records*)malloc(sizeof(Records)*8000);//will hold the data from each line
-
+        input= (Records*)malloc(sizeof(Records)*8000);
         if(argc != 3)
         {
                 printf("You have input the wrong amount of arguments!");
@@ -348,14 +580,13 @@ int main(int argc, char **argv) {
 
         char *boolean= (char*)malloc(sizeof(char)*10);
         strcpy(boolean,"f");
-        //printf("boolean ineginnin %s\n",boolean);
 
         while(strlen(line)>1&& strcmp(boolean,"f")==0) {
+
                 //  printf("i got into the first while loop \n");
                 //  printf("line is %s\n",line);
                 i=0;
                 commas=0;
-
                 while(i<strlen(line)) {
 
                         if(line[i]==',') {
@@ -587,13 +818,24 @@ int main(int argc, char **argv) {
 
                 fgets(line,3000,stdin);
 
-                //printf("line is %s\n",line);
+                char* temp=(char*)malloc(sizeof(char)*3000);
                 if(strcmp(line,second_line)==0) {
                         //  printf("i made it here\n");
 
                         strcpy(boolean,"t");
-                        //printf("noolean is %s\n",boolean);
+                        strcpy(temp,line);
+                        fgets(line,3000,stdin);
+
+                        if(strcmp(line,temp)!=0) {
+                                //call method
+                                strcpy(boolean,"f");
+                                s++;
+                                printf("S before: %d\n", s);
+                                parseExtra(temp, s);
+                        }
                 }
+
+
 
                 strcpy(second_line,line);
 
@@ -618,7 +860,6 @@ int main(int argc, char **argv) {
         printf("\n\n**SORTED**\n");
         printRecord(input, s);
         printf("**Printed**\n");
-        printf("Line 6000: %s\n", input[6000].director_name);
         print_csv_file(&input, arraySize);
 
 
